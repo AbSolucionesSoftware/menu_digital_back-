18e56d7069b6c09de3c1d8c2f6ca09e8ab0efc21
Diego <consultoresartificialbrain@gmail.com>
2021-02-24 09:47:37 -0600

    agregnado todo

--- src/controllers/Company.controller.js
+++ src/controllers/Company.controller.js
@@ -47,6 +47,50 @@ companyCtrl.createCompany = async (req,res) => {
     }
 }
 
+companyCtrl.createCompanyAdmin = async (req,res) => {
+    try {
+        const { password, repeatPassword } = req.body;
+        console.log(req.body);
+        const newCompany = new modelCompany(req.body);
+        newCompany.public = false;
+        newCompany.type = true;
+        if(!password || !repeatPassword){
+            res.status(404).json({ message: "Las contrasenas son obligatorias." });
+        } else {
+            if(password !== repeatPassword){
+                res.status(404).json({ message: "Las contrasenas no son iguales." });
+            } else {
+                bcrypt.hash(password, null, null, function (err, hash){
+                    if(err){
+                        res.status(500).json({message: "Ups, algo paso al registrar el usuario",err,});
+                    } else {
+                        newCompany.password = hash;
+                        newCompany.save((err, userStored) => {
+                            if (err) {
+                              res
+                                .status(500)
+                                .json({
+                                  message: "Ups, algo paso al registrar el usuario",
+                                  err,
+                                });
+                            } else {
+                              if (!userStored) {
+                                res.status(404).json({ message: "Error al crear el usuario" });
+                              } else {
+                                res.status(200).json({message: "Usuario agregado."});
+                              }
+                            }
+                          })
+                    }
+                })
+            }
+        }
+    } catch (error) {
+        res.status(500).json({message: "Error del server", error})
+        console.log(error);
+    }
+}
+
 companyCtrl.editCompany = async (req,res) => {
     try {
         
@@ -56,9 +100,19 @@ companyCtrl.editCompany = async (req,res) => {
     }
 }
 
-companyCtrl.getCompany = async (req,res) => {
+companyCtrl.getCompanys = async (req,res) => {
     try {
         const companys = modelCompany.find({});
+        res.status(200).json(companys);
+    } catch (error) {
+        res.status(500).json({message: "Error del server", error})
+        console.log(error);
+    }
+}
+
+companyCtrl.getCompany = async (req,res) => {
+    try {
+        const companys = modelCompany.findById(req.params.idCompany);
         res.status(200).json(companys);
     } catch (error) {
         res.status(500).json({message: "Error del server", error})
--- src/routes/Company.js
+++ src/routes/Company.js
@@ -1,9 +1,16 @@
 const { Router } = require('express');
 const router = Router();
-const { createCompany, inicioSesion } = require('../controllers/Company.controller');
+const { createCompany, inicioSesion, createCompanyAdmin, getCompanys, getCompany, editCompany } = require('../controllers/Company.controller');
+const auth = require('../middleware/auth');
 
-router.route('/').post(createCompany);
+router.route('/').post(createCompany).get(getCompanys);
+
+router.route('/admin').post(createCompanyAdmin);
 
 router.route('/logIn').post(inicioSesion);
+
+router.route('/:idCompany').post(getCompany).put(editCompany);
+
+
 
 module.exports = router;
