35ffd0c15c6d8f90079099a96a37d49e9be4e273
Diego <consultoresartificialbrain@gmail.com>
2021-02-26 10:02:09 -0600

    acomodando error

--- src/routes/Banner.js
+++ src/routes/Banner.js
@@ -3,7 +3,7 @@ const router = Router();
 const {uploadImagen, createBanner,editBanner,deleteBaneer, getBanner } = require('../controllers/Banner.controller');
 const auth = require('../middleware/auth');
 
-router.route('/').post(uploadImagen,createBanner);
+router.route('/').post(auth,uploadImagen,createBanner);
 
 router.route('/:idBanner').put(auth,editBanner).delete(auth,deleteBaneer);
 
