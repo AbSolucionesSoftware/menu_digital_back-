9a25f827bd6ed6f6b06f8d1023672a05be351015
Diego <consultoresartificialbrain@gmail.com>
2021-03-04 12:46:11 -0600

    agregando todo

--- src/controllers/Product.controller.js
+++ src/controllers/Product.controller.js
@@ -56,7 +56,16 @@ productCtrl.editProduct = async (req,res) => {
 
 productCtrl.deleteProduct = async (req,res) => {
     try {
-        
+        const productBase = await modelProduct.findById(req.params.idProducto);
+        if(productBase){
+            if(productBase.imagenProductKey){
+                upliadImagen.eliminarImagen(productBase.imagenProductKey);
+            }
+            await modelProduct.findByIdAndDelete(req.params.idProducto);
+            res.status(200).json({message: "Producto eliminado."});
+        }else{
+            res.status(404).json({message: "Este producto no existe."});
+        }
     } catch (error) {
         res.status(500).json({message: "Error del servidor"}, error);
         console.log(error);
@@ -65,7 +74,90 @@ productCtrl.deleteProduct = async (req,res) => {
 
 productCtrl.getProductCompany = async (req,res) => {
     try {
-        
+        const productosCompany = await modelProduct.find({company: req.params.idCompany});
+        res.status(200).json(productosCompany);
+    } catch (error) {
+        res.status(500).json({message: "Error del servidor"}, error);
+        console.log(error);
+    }
+}
+
+productCtrl.agruparCategoriasFiltro = async (req,res) => {
+    try {
+        await modelProduct.aggregate(
+			[
+				{
+					$match: {
+						company: req.params.idCompany
+					}
+				},
+				{ $group: { _id: '$category' } }
+			],
+			async function(err, categorias) {
+				arrayCategorias = [];
+				console.log(categorias);
+				console.log(categorias.length);
+				for (i = 0; i < categorias.length; i++) {
+					if (categorias[i]._id !== null) {
+						if (categorias[i]._id) {
+						const subCategoriasBase =await Producto.aggregate(
+								[
+									{
+										$match: {
+											$or: [ { categoria: categorias[i]._id }, { eliminado: { $exists: false } }, { eliminado: false } ]
+										}
+									},
+									{
+										$group: { _id: '$subCategoria' }
+									}
+								],
+								async function(err, subCategoriasBase) {
+									return subCategoriasBase;
+								}
+							);
+							arrayCategorias.push({
+								categoria: categorias[i]._id,
+								subcCategoria: subCategoriasBase
+							});
+						}
+					}
+				}
+				res.status(200).json(arrayCategorias);
+			}
+		);
+    } catch (error) {
+        res.status(500).json({message: "Error del servidor"}, error);
+        console.log(error);
+    }
+}
+
+productCtrl.filtroBusqueda = async (req, res) => {
+    try {
+        const {filter} = req.query;
+        await Producto.aggregate(
+			[
+				{
+					$match: {
+						$or: [
+							{ name: { $regex: '.*' + filter + '.*', $options: 'i' } },
+							{ category: { $regex: '.*' + filter + '.*', $options: 'i' } },
+							{ subCategory: { $regex: '.*' + filter + '.*', $options: 'i' } }
+						]
+					}
+				}
+			],
+			(err, postStored) => {
+				if (err) {
+					res.status(500).json({ message: 'Error en el servidor', err });
+				} else {
+					if (!postStored) {
+						res.status(404).json({ message: 'Error al mostrar Productos' });
+					} else {
+						res.status(200).json(postStored);
+					}
+				}
+			}
+		);
     } catch (error) {
         res.status(500).json({message: "Error del servidor"}, error);
         console.log(error);
--- src/routes/Product.js
+++ src/routes/Product.js
@@ -1,10 +1,14 @@
 const { Router } = require('express');
 const router = Router();
-const { uploadImagen, createProduct, editProduct, getProductCompany, deleteProduct } = require('../controllers/Product.controller');
+const { uploadImagen, createProduct, editProduct, getProductCompany, deleteProduct, agruparCategoriasFiltro, filtroBusqueda } = require('../controllers/Product.controller');
 const auth = require('../middleware/auth');
 
-router.route('/:idCompany').post(auth,createProduct);
+router.route('/:idCompany').post(auth,uploadImagen,createProduct).get(getProductCompany);
 
-router.route('/edit/:idProducto').put(editProduct);
+router.route('/edit/:idProducto').put(auth,uploadImagen,editProduct).delete(auth,deleteProduct);
 
-module.exports = router;
+router.route('/categiories/:idCompany').get(agruparCategoriasFiltro);
+
+router.route('/search/').get(filtroBusqueda);
+
+module.exports = router
