cc00c9788c27178afc3dcf7b92e66e87705867e6
Diego <consultoresartificialbrain@gmail.com>
2021-03-09 17:10:19 -0600

    agregando todo

--- src/controllers/Company.controller.js
+++ src/controllers/Company.controller.js
@@ -123,8 +123,10 @@ companyCtrl.editCompany = async (req,res) => {
 
 companyCtrl.resetPassCompany = async (req,res) => {
     try {
-        const { password, repeatPassword } = req.body;
+        const { password, repeatPassword  } = req.body;
         console.log(req.body);
+        console.log(password);
+        console.log(repeatPassword);
         if(password !== repeatPassword){
             res.status(404).json({ message: "Las contrasenas no son iguales." });
         } else {
